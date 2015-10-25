ea32469e7a80895f1e5b18fd45021b0e834a6c1c
LeongHouHeng <leonghou2001@gmail.com>
2015-10-26 00:19:42 +0800

    get user GPS

--- app/src/main/java/com/blogspot/cartonsbase/chargetogether/Home.java
+++ app/src/main/java/com/blogspot/cartonsbase/chargetogether/Home.java
@@ -1,6 +1,7 @@
 package com.blogspot.cartonsbase.chargetogether;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -11,10 +12,15 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Home extends Activity implements LocationListener {
     private static final String TAG = "Home Debugger";
@@ -29,8 +35,9 @@ public class Home extends Activity implements LocationListener {
     private String bestGPSProvider = LocationManager.GPS_PROVIDER;
     LocationManager locationManager;
     boolean GPSisOPEN = false;
-
+    Location location;
     @Override
+    @TargetApi(23)
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home );
@@ -48,36 +55,34 @@ public class Home extends Activity implements LocationListener {
         if ( locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ||
                 locationManager.isProviderEnabled( LocationManager.NETWORK_PROVIDER ) ) {
             GPSisOPEN = true;
-            GPSSearch();
+        //    GPSSearch();
         }
 
 
     }
 
     public void GPSSearch() {
-
-        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
-            if ( checkSelfPermission( Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager
-                    .PERMISSION_GRANTED && checkSelfPermission( Manifest.permission
-                    .ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-                return;
-            }
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return  ;
         }
         Criteria criteria = new Criteria();
         bestGPSProvider = locationManager.getBestProvider( criteria, true );
 
-        Location location = locationManager.getLastKnownLocation( bestGPSProvider );
+        location = locationManager.getLastKnownLocation( bestGPSProvider );
 
         latitude = location.getLatitude();
         longitude = location.getLongitude();
 
-        Toast.makeText(getApplicationContext(),  "Latitude: " + latitude + "\nLongitude" + longitude, Toast.LENGTH_SHORT).show();
+        Toast.makeText( getApplicationContext(), "Latitude: " + latitude + "\nLongitude" + longitude, Toast.LENGTH_SHORT ).show();
         Log.d( TAG, "Latitude: " + latitude + "\nLongitude" + longitude );
     }
 
     @Override
     public void onLocationChanged( Location location ) {
-        Toast.makeText( getApplicationContext(), "Location Update: " + "\nLatitude: " + latitude + "\nLongitude" + longitude, Toast.LENGTH_SHORT ).show();
+        //Toast.makeText( getApplicationContext(), "Location Update: " + "\nLatitude: " + latitude + "\nLongitude" + longitude, Toast.LENGTH_SHORT ).show();
+        GPSSearch();
     }
 
     @Override
@@ -99,16 +104,22 @@ public class Home extends Activity implements LocationListener {
     protected void onResume() {
         super.onResume();
         if ( GPSisOPEN ) {
-            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
-                if ( checkSelfPermission( Manifest.permission.ACCESS_FINE_LOCATION ) !=
-                        PackageManager.PERMISSION_GRANTED && checkSelfPermission( Manifest.permission
-                        .ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-                    return;
-                }
+            if ( Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return  ;
             }
-            locationManager.requestLocationUpdates( bestGPSProvider, 5000, 1, this );//60000 --> 1 min
-        }
+            locationManager.requestLocationUpdates( bestGPSProvider, 1000, 1, this );
 
+            /*Timer timer = new Timer(  );
+            timer.schedule( new TimerTask() {
+                @Override
+                public void run() {
+
+                }
+            }, 5000 );*/
+
+        }
     }
 
     @Override
